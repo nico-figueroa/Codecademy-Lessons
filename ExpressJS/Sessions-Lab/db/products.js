a107c32caaebe8d55832df33eb17d8be59df63d4
Nomadant <nitem18@hotmail.com>
2026-08-27 14:47:32 -0700

    Addition of session lab files. Update of node modules.

---
+++ ExpressJS/Sessions-Lab/db/products.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    id: 1,
+    name: "Aloe Vera",
+    price: 35,
+    image: "https://imgur.com/40IA6kp.jpg",
+  },
+  {
+    id: 2,
+    name: "Pilea",
+    price: 50,
+    image: "https://imgur.com/s7SbsqX.jpg",
+  },
+  {
+    id: 3,
+    name: "Pilea",
+    price: 50,
+    image: "https://imgur.com/DT489yh.jpg",
+  },
+];
